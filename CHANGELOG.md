78b0340107442a51c774891315312c38adcf72ee
Ian Yu <ian.yu@arc.com.co>
2021-04-27 23:06:23 -0400

    included documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## ## **[Unreleased]**
 - class `PostSalesAnalysis()`
 
+## **[0.1.3] - 2021-03-29**
+### **Changed**
+- Fixed EDA Notebook: `total_growth` parameter represents a seller "grew by how many times", not "percentage" as originally stated
+
 ## **[0.1.2] - 2021-03-26**
 ### **Changed**
 - BUG FIXED: Package Name to eCommerce
--- eCommerce/data.py
+++ eCommerce/data.py
@@ -15,8 +15,24 @@ import pandas as pd
 
 class Olist:
     '''
-    A class that manipulates Olist datasets. Default S3 bucket is ianyu-public-data
+    Manipulates Olist datasets. Default S3 bucket is ianyu-public-data.
+
+    Parameters 
+    ==========
+    bucket: str, S3 bucket name to pull Olist data from. ianyu-public-data by default.
+
+    Attributes
+    ==========
+    bucket: S3 bucket name
+    s3: boto3.resource instance
+    tables: all table names collected through 
+
+    Methods
+    =======
+    table_names(): returns names of all available Olist datasets retrieved 
+    get_table(key): returns a dataset for a given key 
     '''
+
     def __init__(self, bucket = "ianyu-public-data"):
         self.bucket = bucket
         self.s3 = boto3.resource('s3')
@@ -24,9 +40,13 @@ class Olist:
     # Get table names
     def table_names(self, prefix = "olist/olist"):
         '''
-        A function that gets all the available datasets given a prefix on S3 Bucket
+        Gets all the available datasets given a prefix on S3 Bucket
         The default is the default prefix for "ianyu-public-data"
         The result is a dictionary with table names as keys and path as values 
+
+        Parameters
+        ==========
+        prefix: str, prefix on the S3 bucket where Olist's datasets exist
         '''
         # Get names 
         tables = {}
@@ -40,15 +60,24 @@ class Olist:
 
     def get_table(self, key, clean = False):
         '''
-        A function that takes in key argument and path argument
+        Takes in key argument and path argument
         Key argument indicates which dataset to request
         The function returns a Pandas DataFrame
+
+        Parameters
+        ==========
+        key: str, name of the dataset to retrieve
+        clean: bool, if True, activate clean_df to to clean the dataset. False by default.
         '''
         assert isinstance(clean, bool), "clean must be a boolean"
+
+        # Get table
         if not self.tables:
             raise Exception("Tables must not be empty. Run Olist.table_names() to get table names")
         
         df = pd.read_csv(self.s3.Object(bucket_name = self.bucket, key = self.tables[key]).get()['Body'])
+
+        # Activate clean_df()
         if clean == False:
             pass
         elif clean == True:
@@ -59,8 +88,13 @@ class Olist:
 
 def clean_df(df, key):
     '''
-    A function that contains a list of rulesets in order to clean Olist's dataset.
+    Contains a list of rulesets in order to clean Olist's dataset.
     It is made to have rulesets adjsuted according to needs.
+
+    Parameters
+    ==========
+    df: pd.DataFrame, Olist DataFrame to clean
+    key: str, name of the Olist dataset to clean
     '''
 
     # A growing list of custom rules
@@ -131,7 +165,14 @@ def clean_df(df, key):
 
 def single_seller(ps, feature='price', seller_id = None, resample = '1D', function = 'cumsum'):
     '''
-    A custom function that helps examine a seller's performance over time
+    Custom function that helps examine a seller's performance over time
+
+    Parameters
+    ==========
+    feature: str, feature of the DataFrame to analyize. price by default.
+    seller_id: int, seller_id. None by default, which triggers random selection of seller_id
+    resample: str, timeframe to resample. 5M for 5 months, 1Y for 1 year. 1D by default.
+    function: str, aggregate function. choice of daily_sum or daily_mean or cumsum or total_growth
     '''
     if seller_id == None:
         seller = random.choice([x for x in ps['seller_id']])
